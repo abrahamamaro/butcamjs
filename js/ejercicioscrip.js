d971fe63aa72ba7f72088f085d8bb2251df76d28
abraham <abrahamamaro@hotmail.com>
2021-10-01 22:25:29 -0500

    repo js

---
+++ js/ejercicioscrip.js
@@ -0,0 +1,27 @@
+/*EJERCICIO DE COMPARACION*/
+
+//let numeroUno = window.prompt("ingresa un numero")
+//let numeroDos = window.prompt("ingresa un numero")
+
+//if (numeroUno >= numeroDos) {
+//    console.log("es mayor el primero")
+//} else {
+//    console.log("es mayor el segundo")
+//}
+
+/*EJERCICIO DE CALIFICACION*/
+//let Calificacion = window.prompt("ingresa tu calificación")
+
+//if (Calificacion >= 100) {
+//    console.log('alumno A')
+//} else if (Calificacion >= 90 && Calificacion > 80) {
+//    console.log('alumno B')
+//} else if (Calificacion >= 80 && Calificacion > 70) {
+//    console.log('alumno D')
+//} else if (Calificacion >= 70 && Calificacion > 60) {
+//    console.log('alumno D')
+//} else if (Calificacion >= 60 && Calificacion > 50) {
+//    console.log('alumno C')
+//} else {
+//    console.log('es otra calificaion')
+//}
