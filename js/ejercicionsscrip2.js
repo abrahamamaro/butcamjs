b42e4844c9927ab2fa23379aaf458fd7f9bd3f75
abraham <abrahamamaro@hotmail.com>
2021-10-04 21:39:09 -0500

    ejercicios 2

---
+++ js/ejercicionsscrip2.js
@@ -0,0 +1,56 @@
+/*
+Ejercicio 1:
+    Solicitar al usuario su nombre y sus apellidos
+    regresar en una sola cadena donde el nombre sea minusculas y los apellidos sean mayusculas
+    input nombre = "Ivan"
+    input Apellido = "Diaz"
+    Output: "fernanda PALACIOS"
+*/
+/* ------------EJERCICIO 1 -------------------*/
+
+/*const nombre = prompt('ingresa tu nombre');
+const apellidos = prompt('ingresa tus apellidos');
+const unionnombre = nombre.toLowerCase(nombre) + apellidos;
+console.log(unionnombre)*/
+
+
+/*
+Ejercicio 2:
+    Solicitar el nombre completo de un usuario e indicarle cuantos caracteres tiene su nombre
+*/
+/* ------------EJERCICIO 2 -------------------*/
+
+/*const nombre = prompt('ingresa tu nombre completo');
+const parametrosnombre = nombre.length
+console.log(parametrosnombre)*/
+/*
+Ejercicio 3:
+    Solicitar el nombre completo a un usuario e indicarle cuantas vocales tiene.
+*/
+
+/*
+        const nombre = prompt('ingresa tu nombre completo');
+const parametrosnombre = nombre.length
+console.log(parametrosnombre)*/
+/*
+Ejercicio 4:
+    Del texto: 
+    "Cada estudiante tiene su ritmo, cada estudiante tiene su talento, y cada estudiante complementa 
+    al estudiante que tiene a su lado"
+    - Contar cuántas veces se usa la palabra "estudiante" y reemplazar todas las coincidencias 
+        por la palabra "Koder", 
+    - y mostrar el mensaje de nuevo al usuario
+*/
+
+/* ------------EJERCICIO 4 -------------------*/
+
+/*let oracion = ("Cada estudiante tiene su ritmo, cada estudiante tiene su talento, y cada estudiante complementa al estudiante que tiene a su lado");
+const sustitucion = oracion.replace(/estudiante/g, "koder");
+console.log(sustitucion)*/
+
+/*
+Ejercicio 5:
+    Dado un string de dos palabras realizar una función que devuelva la palabra más larga
+    input: 'Programación Javascript'
+    Output: 'Programación'
+*/
